488f98e5a39f2ef3f47ed3b0068d4d493a2f9f16
Nitish Kumar Gupta <76789784+Nitish252000@users.noreply.github.com>
2025-12-17 15:46:27 +0530

    Implement Ransom Note solution using hashmap

---
+++ HashMap/Ransom Note.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        if (magazine.size() < ransomNote.size())
+            return false;
+
+        unordered_map<char, int> fq_mz;
+        for (auto s : magazine) {
+            fq_mz[s]++;
+        }
+
+        for (auto i : ransomNote) {
+            if (fq_mz.count(i) && fq_mz[i] != 0) {
+                fq_mz[i]--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
